2209bc27a285b74ecfffcab789ec796d17457e7d
Oaklanz <69703980+Oaklanz@users.noreply.github.com>
2021-05-02 22:16:46 -0400

    movie api assignment

---
+++ controller/movies.js
@@ -0,0 +1,29 @@
+const movies = require('../movies.js')
+
+const getAllMovies = (request, response) => {
+  return response.send(movies)
+}
+
+const getByString = (request, response) => {
+  const { searchString } = request.params
+    const foundDirectors = movies.filter((movie) => {
+      return movie.directors.includes(searchString) ||
+       movie.title.includes(searchString)
+    })
+
+    return response.send(foundDirectors)
+  }
+
+  const saveNewMovie = (request, response) => {
+console.log(request.body)
+    //const {movies} = request.movies
+
+  }
+
+
+
+module.exports = {
+    getAllMovies,
+    getByString,
+    saveNewMovie
+}
